0089e195e042c5ef84a6cd35c46a39e0762ba156
unknown <ivodsalmeida@gmail.com>
2019-02-06 09:19:49 -0200

    Criação do arquivo que adiciona o nome, o preço do produto e informa na msg.

---
+++ loja/adiciona-produto.php
@@ -0,0 +1,9 @@
+<html>
+    <head></head>
+    <body>
+          <?php
+          $nome = $_GET["nome"];
+          $preco = $_GET["preco"];?>
+        Produto, <?= $nome;?> com o preço $<?= $preco;?> adicionado com  sucesso!!
+    </body>
+</html>
