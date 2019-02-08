293f4cc501c980d1645a03225c0b0f1ae105c59c
unknown <ivodsalmeida@gmail.com>
2019-02-08 08:57:41 -0200

    cria arquivo onde vai add através de um formulário o produto

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -4,6 +4,6 @@
           <?php
           $nome = $_GET["nome"];
           $preco = $_GET["preco"];?>
-        Produto, <?= $nome;?> com o preço $<?= $preco;?> adicionado com  sucesso!!
+        Produto <?= $nome;?> com o preço $<?= $preco;?> adicionado com  sucesso!!
     </body>
 </html>
---
+++ loja/produto-formulario.php
@@ -0,0 +1,13 @@
+<html>
+    <head></head>
+    <body>
+        <form action="adiciona-produto.php">
+            Nome:
+            <input type="text" name="nome"><br>
+            Preço:
+            <input type="number" name="preco"><br>
+
+            <input type="submit" value="cadastra">
+        </form>
+    </body>
+</html>
