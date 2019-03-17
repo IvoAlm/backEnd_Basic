272d8732d6dcc72642632e6ffa4317591eb196d2
unknown <ivodsalmeida@gmail.com>
2019-03-17 20:38:30 -0300

    Melhora o codigo add uma funcao para inseri e retornar o resultado da insercao

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -1,11 +1,14 @@
 <?php include('cabecalho.php'); ?>
-    <?php
+ <?php
+    function insereProduto($conexao, $nome, $preco){
+    $query = "Insert into produtos(nome, preco) values ('{$nome}',{$preco})";
+    return mysqli_query($conexao,$query);
+    }
     $nome = $_GET["nome"];
     $preco = $_GET["preco"];
-    $query = "Insert into produtos(nome, preco) values ('{$nome}',{$preco})";
     $conexao = mysqli_connect('localhost','root','', 'loja');
-    mysqli_query($conexao,$query);
-    if (mysqli_query($conexao,$query)){ ?>
+
+    if (insereProduto($conexao, $nome, $preco)){ ?>
         <p class="alert-success">Produto <?= $nome;?> com o preço $<?= $preco;?>, adicionado com  sucesso!!</p>
     <?php } else{ ?>
         <p class="alert-danger">Produto <?= $nome;?> não foi adicionado com  sucesso!!</p>
