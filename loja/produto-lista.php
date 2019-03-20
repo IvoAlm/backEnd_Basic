81d4e1d4126a3b6c47b2d55c26061a26ea6e0022
unknown <ivodsalmeida@gmail.com>
2019-03-20 06:53:43 -0300

    refatora o codigo

--- loja/produto-lista.php
+++ loja/produto-lista.php
@@ -1,12 +1,34 @@
 <?php
 include ("cabecalho.php");
 include ("conecta.php");
-
-$query = "SELECT * FROM loja.produtos;";
-$resultado = mysqli_query($conexao,$query);
-
-while($produto = mysqli_fetch_assoc($resultado)){
-    echo $produto['nome']."<br>";
+function listaProdutos($conexao){
+    $produtos = array();
+    $query = "SELECT * FROM loja.produtos;";
+    $resultado = mysqli_query($conexao,$query);
+    while($produto = mysqli_fetch_assoc($resultado)){
+        array_push($produtos, $produto);
+    }
+    return $produtos;
 }
+?>
+<table class="table table-striped table-bordered">
+    <?php
+        $produtos = $this->listaProduto($conexao);
+        foreach ($produtos as $produto):
+    ?>
+    <tr>
+        <td><?= $produto['nome'] ?></td>
+        <td><?= $produto['preco'] ?></td>
+    </tr>
+    <?php
+        endforeach
+    ?>
+</table>
+<?php include('rodape.php'); ?>
+
+
+
+
+
 
 include("rodape.php");
