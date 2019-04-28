549a6478e66301a571eb54f6482d0f6d15191863
unknown <ivodsalmeida@gmail.com>
2019-04-28 10:55:56 -0300

    Add funcao remove produto e mensagem de sucesso

--- loja/remove-produto.php
+++ loja/remove-produto.php
@@ -2,3 +2,7 @@
 include('conecta.php');
 include('banco-produto.php');
  include('rodape.php');
+ $id = $_GET['id'];
+ removeProduto($conexao, $id);?>
+<p class="text-success"> Produto <?=$id?> removido com sucesso!!!</p>
+
